4a1af9e45629347d98d53699a516ec24ca231867
segasle <segasle@gmail.com>
2019-03-02 10:57:24 +0300

    добавил бургер

--- tempate/header.php
+++ tempate/header.php
@@ -18,6 +18,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css?t=<?php echo(microtime(true) . rand()); ?>">
+    <link rel="stylesheet" href="icons/css/font-awesome.min.css">
     <script type="text/javascript">
         (function (d, w, c) {
             (w[c] = w[c] || []).push(function () {
@@ -72,7 +73,14 @@
 <description>
     <div class="heder">
         <div class="cap">
+            <div class="menu">
+                <input type="checkbox" id="checkbox">
+                <label class="burger" for="checkbox">
+                    <div class="fa fa-5x fa-bars"></div>
+                </label>
                 <?php echo menu();?>
+
+            </div>
 
     </div>
 </description>
