d31f34fa1c2ad92f0d79906f23567a5c3ea1818f
segasle <segasle@gmail.com>
2019-03-04 20:19:38 +0300

    структура корзины

--- upmenu/basket.php
+++ upmenu/basket.php
@@ -1,4 +1,36 @@
 <h1 class="text-center">Корзина</h1>
+<div class="table">
+    <div class="table-cell">
+        <div class="block">
+            <div class="block_title"></div>
+            <div class="block_description"></div>
+        </div>
+    </div>
+    <div class="table-cell">
+        <div class="block">
+            <div class="block_title"></div>
+            <div class="block_description"></div>
+        </div>
+    </div>
+    <div class="table-cell">
+        <div class="block">
+            <div class="block_title"></div>
+            <div class="block_description"></div>
+        </div>
+    </div>
+    <div class="table-cell">
+        <div class="block">
+            <div class="block_title"></div>
+            <div class="block_description"></div>
+        </div>
+    </div>
+    <div class="table-cell">
+        <div class="block">
+            <div class="block_title"></div>
+            <div class="block_description"></div>
+        </div>
+    </div>
+</div>
 <?php
 /**
  * Created by PhpStorm.
