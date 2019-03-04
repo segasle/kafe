fa353373ec6b0f01b8f4f7db85a0966f67e7ed4e
sergej <segasle@gmail.com>
2019-03-04 21:29:35 +0300

    сделал шаблон корзины

--- upmenu/basket.php
+++ upmenu/basket.php
@@ -1,40 +1,28 @@
 <h1 class="text-center">Корзина</h1>
 <div class="table">
-    <div class="table-cell">
-        <div class="block">
-            <div class="block_title"></div>
-            <div class="block_description"></div>
-        </div>
-    </div>
-    <div class="table-cell">
-        <div class="block">
-            <div class="block_title"></div>
-            <div class="block_description"></div>
-        </div>
-    </div>
-    <div class="table-cell">
-        <div class="block">
-            <div class="block_title"></div>
-            <div class="block_description"></div>
-        </div>
-    </div>
-    <div class="table-cell">
-        <div class="block">
-            <div class="block_title"></div>
-            <div class="block_description"></div>
-        </div>
-    </div>
-    <div class="table-cell">
-        <div class="block">
-            <div class="block_title"></div>
-            <div class="block_description"></div>
-        </div>
-    </div>
-</div>
 <?php
-/**
- * Created by PhpStorm.
- * User: sergej
- * Date: 04.03.19
- * Time: 17:04
- */
+$array = array(
+    array(
+        'head' => 'Изображения',
+    ),
+    array(
+        'head' => 'Названия',
+    ),
+    array(
+        'head' => 'Количество',
+    ),
+    array(
+        'head' => 'Сумма',
+    ),
+    array(
+        'head' => 'Итого',
+    ),
+);
+foreach ($array as $item) {
+    echo "<div class=\"table-cell\">
+        <div class=\"block\">
+            <div class=\"block_title\"><p>".$item['head']."</p></div>
+            <div class=\"block_description\"></div>
+        </div>
+    </div>";
+}
