10f17821b63db68d7763b3860f1c6143a58085b0
segasle <segasle@gmail.com>
2019-03-05 13:39:07 +0300

    подготовка входв и регистрации

--- upmenu/reg.php
+++ upmenu/reg.php
@@ -1,23 +1,26 @@
-<?php echo registration(); ?>
+<?php  echo registration(); ?>
 <h1 class="text-center">Регистрация</h1>
 <form action="" id="registration-form" class="form-reg" method="post">
     <div class="form-group">
         <label for="exampleInputEmail1" class="point">Ваш email</label>
-        <input type="text" class="form-control" name="email"  id="exampleInputEmail1" placeholder="Email"
+        <input type="text" class="form-control" name="email" id="exampleInputEmail1" placeholder="Email"
                data-validation="email"
                data-validation-error-msg="Некорретно введина почта">
     </div>
     <div class="form-group">
         <label for="exampleInputLogin2" class="point">Придумаете логин</label>
-        <input type="text" class="form-control" id="exampleInputLogin2" value="<?php // echo @$data['login'];?>" name="login" placeholder="Логин"
+        <input type="text" class="form-control" id="exampleInputLogin2" value="<?php  $_POST['login'];?>"
+               name="login" placeholder="Логин"
                data-validation="length alphanumeric"
                data-validation-length="3-12"
                data-validation-error-msg="Введите имя длинее (3-12 симлов)">
     </div>
     <div class="form-group">
         <label for="exampleInputPassword1" class="point">Придумаете пароль</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" value="<?php //echo @$data['password1'];?>" name="pass_confirmation" placeholder="Пароль"
-               data-validation="length strength" data-validation-length="min6" data-validation-error-msg="Короткий пароль, должно быть не меньше 6 симлов"
+        <input type="password" class="form-control" id="exampleInputPassword1"
+                name="pass_confirmation" placeholder="Пароль"
+               data-validation="length strength" data-validation-length="min6"
+               data-validation-error-msg="Короткий пароль, должно быть не меньше 6 симлов"
                data-validation-strength="2">
     </div>
     <div class="form-group">
@@ -32,21 +35,24 @@
             Вы должны согласится со правами на <a target="_blank" href="">сборку персоналых данных</a>
         </label>
     </div>
-    <button type="submit" name="event" class="btn btn-default" onclick="form(document.getElementById('registration-form'))">Отправить</button>
+    <button type="submit" name="event" class="btn btn-default"
+            onclick="form(document.getElementById('registration-form'))">Отправить
+    </button>
 </form>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script><script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
+<script>
 
     $.validate({
-        modules : 'location, date, security, file',
-        onModulesLoaded : function() {
+        modules: 'location, date, security, file',
+        onModulesLoaded: function () {
             $('#country').suggestCountry();
         }
     });
 
     // Restrict presentation length
-    $('#presentation').restrictLength( $('#pres-max-length') );
+    $('#presentation').restrictLength($('#pres-max-length'));
     $.validate({
-        modules : 'security'
+        modules: 'security'
     });
 </script>
