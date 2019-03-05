2b7e1612c4cd7b30a0672efc279cb0110fcb7b02
sergej <segasle@gmail.com>
2019-03-05 10:44:21 +0300

    добавил второй таблицу

--- upmenu/basket.php
+++ upmenu/basket.php
@@ -1,28 +1,56 @@
 <h1 class="text-center">Корзина</h1>
 <div class="table">
-<?php
-$array = array(
-    array(
-        'head' => 'Изображения',
-    ),
-    array(
-        'head' => 'Названия',
-    ),
-    array(
-        'head' => 'Количество',
-    ),
-    array(
-        'head' => 'Сумма',
-    ),
-    array(
-        'head' => 'Итого',
-    ),
-);
-foreach ($array as $item) {
-    echo "<div class=\"table-cell\">
+    <?php
+    $array = array(
+        array(
+            'head' => 'Изображения',
+        ),
+        array(
+            'head' => 'Названия',
+        ),
+        array(
+            'head' => 'Количество',
+        ),
+        array(
+            'head' => 'Сумма',
+        ),
+        array(
+            'head' => 'Итого',
+        ),
+    );
+    foreach ($array as $item) {
+        echo "<div class=\"table-cell\">
         <div class=\"block\">
-            <div class=\"block_title\"><p>".$item['head']."</p></div>
+            <div class=\"block_title\"><p>" . $item['head'] . "</p></div>
             <div class=\"block_description\"></div>
         </div>
     </div>";
-}
+    }
+    ?>
+</div>
+<div class="float-right">
+    <div class="table">
+        <?php
+        $basket = array(
+            array(
+                'head' => 'Скидка',
+            ),
+            array(
+                'head' => 'Сумма',
+            ),
+            array(
+                'head' => 'Итого',
+            ),
+        );
+        foreach ($basket as $item) {
+
+            echo "<div class=\"table-cell\">
+        <div class=\"block\">
+            <div class=\"block_title\"><p>" . $item['head'] . "</p></div>
+            <div class=\"block_description\"></div>
+        </div>
+    </div>";
+        }
+        ?>
+    </div>
+</div>
