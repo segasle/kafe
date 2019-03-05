0f520a5ab878ccdda233f7e61098b64fcfeb75b7
sergej <segasle@gmail.com>
2019-03-05 16:33:09 +0300

    добавил 2 поля

--- upmenu/reg.php
+++ upmenu/reg.php
@@ -24,11 +24,25 @@ $array = array(
         'placeholder' => 'Пароль',
     ),
     array(
-        'head' => '',
-        'for' => '',
-        'type' => '',
-        'name' => '',
-        'placeholder' => '',
+        'head' => 'Подтведите пароль',
+        'for' => 'exampleInputPassword2',
+        'type' => 'password',
+        'name' => 'password2',
+        'placeholder' => 'Пароль',
+    ),
+    array(
+        'head' => 'Номер телефона',
+        'for' => 'exampleInputTel',
+        'type' => 'tel',
+        'name' => 'phone',
+        'placeholder' => 'Телефон',
+    ),
+    array(
+        'head' => 'Адрес',
+        'for' => 'exampleInputAddress',
+        'type' => 'text',
+        'name' => 'address',
+        'placeholder' => 'Адрес',
     ),
 );
 
@@ -49,7 +63,5 @@ $array = array(
             Вы должны согласится со правами на <a target="_blank" href="">сборку персоналых данных</a>
         </label>
     </div>
-    <button type="submit" name="event" class="btn btn-default"
-            onclick="form(document.getElementById('registration-form'))">Отправить
-    </button>
+    <button type="submit" name="event" class="btn btn-default">Отправить</button>
 </form>
