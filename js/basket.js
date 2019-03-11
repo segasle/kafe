d792e1736a04649d7ed82edf1edf3ad793fb18a0
segasle <segasle@gmail.com>
2019-03-11 21:10:56 +0300

    несколько значений в корзину

--- js/basket.js
+++ js/basket.js
@@ -1,6 +1,13 @@
+var car = {};
 $('.js-basket').on('click', basket);
 
 function basket() {
     var id =  $(this).attr('data-id');
-    console.log(id);
+    //console.log(id);
+    if (car[id] == undefined){
+        car[id] = 1;
+    }else {
+        car[id]++;
+    }
+    console.log(car[id]);
 }
