c6b7aef82cd0527ccf3a0e7204705480c99c3b07
sergej <segasle@gmail.com>
2019-03-11 17:00:42 +0300

    должно добавлять +1 продукт в корзину , но не получилось

--- basket.php
+++ basket.php
@@ -1,6 +1,12 @@
 <?php
-addbasket();
+
 if (isset($_SESSION['id'])){
+    $_SESSION['id'] = array();
+    if (empty($_SESSION['id'])){
+        $_SESSION['id'] = 1;
+    }else{
+        $_SESSION['id']+=1;
+    }
     $out = '<form action="" method="post">
     <button type="submit" name="basket" class="basket"><i class="fa fa-shopping-basket fa-2x"></i><p>'.$_SESSION['id'].'</p></button>
 </form>';
@@ -9,4 +15,4 @@ if (isset($_SESSION['id'])){
 }
 echo $out;
 
-?>
+?>                                                                                                                                                                      +
