de6325eca3d91cde8f1c16ea6bf7e95821bd33e1
sergej <segasle@gmail.com>
2019-03-15 19:33:40 +0300

    Вывод корзины товаров

---
+++ functions/basket.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergej
+ * Date: 15.03.19
+ * Time: 19:01
+ */
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    require 'db.php';
+    require 'functions.php';
+    $res = do_query("SELECT * FROM `products` JOIN `cart` WHERE `cat.ip` = '{$_SERVER['REQUEST_METHOD']}' AND `products.idd` = `cart.id_products`");
+    if (mysqli_num_rows($res) > 0) {
+        $row = mysqli_fetch_array($res);
+        do{
+            $count = $count + $row['count'];
+        }while($row = mysqli_fetch_array($res));
+        echo $count;
+    }
+}
