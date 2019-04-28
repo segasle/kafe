d3ebb530ca1f2c9ac7b3e24c8e8f9714a08270ae
sergej <segasle@gmail.com>
2019-04-28 19:49:36 +0300

    исправил слайдер

--- upmenu/main.php
+++ upmenu/main.php
@@ -1,29 +1,26 @@
-<h1 class="text-center">Скидки, акции</h1><!-- Bootstrap 3 -->
-<div id="carousel" class="carousel slide" data-ride="carousel">
-    <!-- Индикаторы -->
+<h1 class="text-center">Скидки, акции</h1>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-        <li data-target="#carousel" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel" data-slide-to="1"></li>
-        <li data-target="#carousel" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
-        <div class="item active">
-            <img src="photo/original.jpg" alt="...">
+        <div class="carousel-item active">
+            <img src="photo/original.jpg" class="d-block w-100" alt="...">
         </div>
-        <div class="item">
-            <img src="photo/rt.jpg" alt="...">
+        <div class="carousel-item">  <img  class="d-block w-100" src="photo/rt.jpg" alt="...">
         </div>
-        <div class="item">
-            <img src="photo/granada.jpg" alt="...">
+        <div class="carousel-item">
+            <img src="photo/granada.jpg" class="d-block w-100" alt="...">
         </div>
     </div>
-    <!-- Элементы управления -->
-    <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Предыдущий</span>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
     </a>
-    <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Следующий</span>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
     </a>
 </div>
